19d5f231547e9e10a7fcba813eaf4d1a36ca21d7
Nigel Feng <nfeng29@gmail.com>
2025-06-21 17:32:20 +1000

    input radio selector

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 // product array-objects
 // data structure - same structure as html
@@ -76,7 +77,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
