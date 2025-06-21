d7536349b606929cfc3b196472da3267fd28545c
Nigel Feng <nfeng29@gmail.com>
2025-06-21 12:30:35 +1000

    add cart.js, data attribute, button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,51 @@
 // product array-objects
 // data structure - same structure as html
+
+/*
+1. save data
+2. generate html
+3. make it interactive
+*/
+
 // 1. save data
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Size Basketball",
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799
-    }
-];
+// const products = [
+//     {
+//         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//         rating: {
+//             stars: 4.5,
+//             count: 87
+//         },
+//         priceCents: 1090
+//     },
+//     {
+//         image: "images/products/intermediate-composite-basketball.jpg",
+//         name: "Intermediate Size Basketball",
+//         rating: {
+//             stars: 4,
+//             count: 127
+//         },
+//         priceCents: 2095
+//     },
+//     {
+//         image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//         name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//         rating: {
+//             stars: 4.5,
+//             count: 56
+//         },
+//         priceCents: 799
+//     },
+//     {
+//         image: "images/products/black-2-slot-toaster.jpg",
+//         name: "2 Slot Toaster - Black",
+//         rating: {
+//             stars: 5,
+//             count: 2197
+//         },
+//         priceCents: 1899
+//     },
+// ];
 
 let productHTML = "";
 
@@ -82,14 +98,46 @@ products.forEach((product) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button
+                button-primary js-add-to-cart"
+                data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
     `;
 });
 
-console.log(productHTML);
+// console.log(productHTML);
 
 // DOM - select html
 document.querySelector(".js-products-grid").innerHTML = productHTML;
+
+// 3. interactive
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+    // data attribute - data-product-name="" Kebab-case
+    // attach information to html element
+    // kebab to camel case
+        const productId = button.dataset.productId;
+        
+        let matchingItem;
+        
+        // check if in cart
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        // match increase quantity
+        if (matchingItem) {
+            matchingItem.quantity = matchingItem.quantity + 1;
+        } else {
+            cart.push({
+            productId: productId,
+            quantity: 1,
+            });
+        }
+        console.log(cart);
+    });
+});
