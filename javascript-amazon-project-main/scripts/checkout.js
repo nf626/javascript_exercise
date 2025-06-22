06313c740eae19d4b60ec7b83e4f82ddebbe9b2e
Nigel Feng <nfeng29@gmail.com>
2025-06-22 22:57:44 +1000

    promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,129 +2,13 @@ import { cart, removeCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/backend-practice.js";
 // import "../data/cart-class.js"
 
 // input type="radio" - same name value can only pick one
 // different can choose both.
 
-loadProducts(() => {
-    let cartSumamryHTML = "";
-
-    cart.forEach((cartItem) => {
-        const productId = cartItem.productId;
-
-        let matchingProduct;
-
-        products.forEach((product) => {
-            if (product.id === productId) {
-                matchingProduct = product;
-            }
-        });
-
-        // console.log(matchingProduct);
-
-        // 2. generate html
-        cartSumamryHTML = cartSumamryHTML +
-        `
-        <div class="cart-item-container
-            js-cart-item-container-${matchingProduct.id}">
-            <div class="delivery-date">
-                Delivery date: Tuesday, June 21
-            </div>
-
-            <div class="cart-item-details-grid">
-                <img class="product-image"
-                src="${matchingProduct.image}">
-
-                <div class="cart-item-details">
-                <div class="product-name">
-                    ${matchingProduct.name}
-                </div>
-                <div class="product-price">
-                    ${matchingProduct.getPrice()}
-                </div>
-                <div class="product-quantity">
-                    <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-                    </span>
-                    <span class="update-quantity-link link-primary">
-                    Update
-                    </span>
-                    <span class="delete-quantity-link link-primary
-                    js-delete-link" data-product-id="${matchingProduct.id}">
-                    Delete
-                    </span>
-                </div>
-                </div>
-
-                <div class="delivery-options">
-                <div class="delivery-options-title">
-                    Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                    <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                    <div>
-                    <div class="delivery-option-date">
-                        Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                        FREE Shipping
-                    </div>
-                    </div>
-                </div>
-                <div class="delivery-option">
-                    <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                    <div>
-                    <div class="delivery-option-date">
-                        Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                        $4.99 - Shipping
-                    </div>
-                    </div>
-                </div>
-                <div class="delivery-option">
-                    <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                    <div>
-                    <div class="delivery-option-date">
-                        Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                        $9.99 - Shipping
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-        `;
-    });
-
-    // 2. generate html
-    document.querySelector(".js-order-summary").innerHTML = cartSumamryHTML;
-
-    document.querySelectorAll(".js-delete-link").forEach((deleteLink) => {
-        deleteLink.addEventListener("click", () => {
-            const productId = deleteLink.dataset.productId;
-
-            removeCart(productId);
-
-            const container = document.querySelector(
-                `.js-cart-item-container-${productId}`
-            );
-
-            container.remove();
-        });
-    });
-
-});
 
 // let cartSumamryHTML = "";
 
@@ -240,3 +124,172 @@ loadProducts(() => {
 //         container.remove();
 //     });
 // });
+
+
+// loadProducts(() => {
+//     loadCart(() => {
+//         let cartSumamryHTML = "";
+
+//         cart.forEach((cartItem) => {
+//             const productId = cartItem.productId;
+
+//             let matchingProduct;
+
+//             products.forEach((product) => {
+//                 if (product.id === productId) {
+//                     matchingProduct = product;
+//                 }
+//             });
+
+//             // console.log(matchingProduct);
+
+//             // 2. generate html
+//             cartSumamryHTML = cartSumamryHTML +
+//             `
+//             <div class="cart-item-container
+//                 js-cart-item-container-${matchingProduct.id}">
+//                 <div class="delivery-date">
+//                     Delivery date: Tuesday, June 21
+//                 </div>
+
+//                 <div class="cart-item-details-grid">
+//                     <img class="product-image"
+//                     src="${matchingProduct.image}">
+
+//                     <div class="cart-item-details">
+//                     <div class="product-name">
+//                         ${matchingProduct.name}
+//                     </div>
+//                     <div class="product-price">
+//                         ${matchingProduct.getPrice()}
+//                     </div>
+//                     <div class="product-quantity">
+//                         <span>
+//                         Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+//                         </span>
+//                         <span class="update-quantity-link link-primary">
+//                         Update
+//                         </span>
+//                         <span class="delete-quantity-link link-primary
+//                         js-delete-link" data-product-id="${matchingProduct.id}">
+//                         Delete
+//                         </span>
+//                     </div>
+//                     </div>
+
+//                     <div class="delivery-options">
+//                     <div class="delivery-options-title">
+//                         Choose a delivery option:
+//                     </div>
+//                     <div class="delivery-option">
+//                         <input type="radio" checked
+//                         class="delivery-option-input"
+//                         name="delivery-option-${matchingProduct.id}">
+//                         <div>
+//                         <div class="delivery-option-date">
+//                             Tuesday, June 21
+//                         </div>
+//                         <div class="delivery-option-price">
+//                             FREE Shipping
+//                         </div>
+//                         </div>
+//                     </div>
+//                     <div class="delivery-option">
+//                         <input type="radio"
+//                         class="delivery-option-input"
+//                         name="delivery-option-${matchingProduct.id}">
+//                         <div>
+//                         <div class="delivery-option-date">
+//                             Wednesday, June 15
+//                         </div>
+//                         <div class="delivery-option-price">
+//                             $4.99 - Shipping
+//                         </div>
+//                         </div>
+//                     </div>
+//                     <div class="delivery-option">
+//                         <input type="radio"
+//                         class="delivery-option-input"
+//                         name="delivery-option-${matchingProduct.id}">
+//                         <div>
+//                         <div class="delivery-option-date">
+//                             Monday, June 13
+//                         </div>
+//                         <div class="delivery-option-price">
+//                             $9.99 - Shipping
+//                         </div>
+//                         </div>
+//                     </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             `;
+//         });
+
+//         // 2. generate html
+//         document.querySelector(".js-order-summary").innerHTML = cartSumamryHTML;
+
+//         document.querySelectorAll(".js-delete-link").forEach((deleteLink) => {
+//             deleteLink.addEventListener("click", () => {
+//                 const productId = deleteLink.dataset.productId;
+
+//                 removeCart(productId);
+
+//                 const container = document.querySelector(
+//                     `.js-cart-item-container-${productId}`
+//                 );
+
+//                 container.remove();
+//             });
+//         });
+//     });
+// });
+
+
+/* Promises */
+// runs immediately, resolve = controls when to go to next step.
+// Promise = constructor, resolve = function
+// new Promise((resolve) => {
+//     console.log("start");
+//     loadProducts(() => {
+//         console.log("finish loading");
+//         resolve("value 1"); // give a value
+//     });
+
+// }).then((value) => {
+//     console.log("next step");
+//     console.log(value);
+
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             console.log("step 2");
+//             resolve();
+//         });
+//     });
+
+//     // renderOrderSummary();
+//     // renderPaymentSummary();
+// }).then(() => {
+//     console.log("third step");
+// });
+
+// Promise.all() - let's us run multiple promises at the same time.
+// wait for all to finish.
+Promise.all([
+        new Promise((resolve) => {
+            loadProducts(() => {
+                console.log("finish loading");
+                resolve("value 1"); // give a value
+        });
+    }),
+        new Promise((resolve) => {
+            loadCart(() => {
+                console.log("step 2");
+                resolve("value 2");
+        });
+    })
+
+]).then((values) => {
+    console.log(values);
+    console.log("all promises finished");
+});
